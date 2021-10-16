a118d8b15d28f5d0e58fb6d386c15240fb8f67f5
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2021-10-16 17:44:41 +0300

    change content on click

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 //import logo from './logo.svg';
 //import './App.css';
 import {useState} from 'react';
-
 export const App = () => {
-    const [isActiv, setActive] = useState(false);
-    const colorControl = () => {
-        setActive(!isActiv)
+    const [content, setContent] = useState('Hello!');
+    const handlerClick = () => {
+        setContent('Click happend!')
     };
     return (
-        <div onClick={colorControl} style={{color: isActiv && 'red'}}>
-            Hello!
+        <div onClick={handlerClick}>
+            {content}
         </div>
     );
 };
