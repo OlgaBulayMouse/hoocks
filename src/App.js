ccdd86d65f7739aefea91756f5ff8c31eaccc996
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2021-10-16 20:38:52 +0300

    use useMemo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 //import logo from './logo.svg';
 //import './App.css';
-import {useState} from 'react';
+import {
+    useMemo,
+    useState
+} from 'react';
+
+export const RandomNumber = ({isShowRandom}) => {
+    const r = useMemo(() => {
+        const Arr = [];
+        const NumsofArray = 9999999;
+        for (let i = 0; i < NumsofArray; i++) {
+            Arr.push(Math.random())
+        };
+        return [Math.round(Math.random() * NumsofArray)]
+    }, []);
+    return (
+        isShowRandom ? (
+        <div>
+            {r}
+        </div>
+        ) : null
+    );
+};
 export const App = () => {
-    const [content, setContent] = useState('Hello!');
+    const [isShowRandom, setIsShowRandom] = useState(false);
     const handlerClick = () => {
-        setContent('Click happend!')
+        setIsShowRandom(!isShowRandom)
     };
     return (
         <div onClick={handlerClick}>
-            {content}
+            Hello! I am reactComponent!
+            <RandomNumber isShowRandom={isShowRandom}/>
         </div>
     );
 };
