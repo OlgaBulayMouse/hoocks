e1a5f7c4fc85c66e221587b958460372b560e581
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2021-10-18 22:47:22 +0300

    hoock useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-//import logo from './logo.svg';
-//import './App.css';
 import {
+    useEffect,
     useState
 } from 'react';
 
 
+
 export const Users = (isShow) => {
     let [users, setUsers] = useState([]);
-    if(!users.length) {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(data => {
-            setUsers(data)
-        });
-    }
+            .then(res => res.json())
+            .then(data => {
+                setUsers(data)
+            }, []);
+    });
     return (
         <div >
             {isShow && users.map(user => user.name)}
@@ -22,11 +22,11 @@ export const Users = (isShow) => {
 };
 
 export const App = () => {
-const [isShow, setIsShow] = useState();
+    const [isShow, setIsShow] = useState();
     return (
         <div onClick={() => setIsShow(!isShow)}>
-            {!isShow && 'Click to fetch users'}
-            <Users isShow={isShow}/>
+            {!isShow && 'Click to fetch users' ||
+            <Users isShow={isShow} />}
         </div>
     );
 };
