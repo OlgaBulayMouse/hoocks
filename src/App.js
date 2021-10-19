466911ec1a21d467647c5e01ff765842835b6b7e
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2021-10-19 18:56:30 +0300

    requesr email users on click

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import {
     useState
 } from 'react';
 
-export const User = ({ name }) => {
+export const User = ({ id, name }) => {
+    const [data, setData] = useState();
+    const [isActive, setIsActive] = useState(false);
+
+    const handlerClick = () => {
+        setIsActive(!isActive);
+    };
+
+    useEffect(() => {
+        if (isActive) {
+            fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+                .then(res => res.json())
+                .then(userData => {
+                    setData(userData)
+                }, [isActive, id])
+        }
+    });
     return (
-        <li>
+        <li onClick={handlerClick}>
             {name}
+
+            {isActive && data && (
+                <div>
+                    {data.email}
+                </div>
+            )}
+
         </li>
     )
-}
+};
 
 export const Users = () => {
     let [users, setUsers] = useState([]);
@@ -26,6 +49,7 @@ export const Users = () => {
                 {users.map(user =>
                     <User
                         key={user.id}
+                        id={user.id}
                         name={user.name}
                     />)
                 }
